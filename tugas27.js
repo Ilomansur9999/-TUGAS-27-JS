11614446a0c643bdf5c3f9b27d916b961eb84977
ilomansur9999 <ilofaqot@gmail.com>
2020-05-06 08:42:27 +0700

    TUGAS 27 JS
    Tugas ini dikumpulkan ke NIOMIC

---
+++ tugas27.js
@@ -0,0 +1,55 @@
+
+function panggilString(){
+	var a = "No ";
+	var b = "rumah saya ";
+	var c = 30;
+	var d = "A";
+	
+	var e = ["No", "rumah saya", 30, "A"];
+	// var b = “Jalan pangeran Tirtayasa No”, 70;
+
+
+
+	hasil = a + b + String(c) + d;
+	console.log(hasil);
+	console.log(e.join(" "));
+
+
+}
+
+panggilString();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function panggilString(){
+// 	var a = ["No", "rumah saya", 30, "A"];
+// 	// var a = “No”,” rumah saya”,30, “A”;
+// 	// var b = “Jalan pangeran Tirtayasa No”, 70;
+
+
+// 	hasil = String(a);
+// 	console.log(hasil);
+
+// }
+
+// panggilString();
+
+
+
